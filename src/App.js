3d32350b7d5ab0b8599534f9f747c7759ba1cddc
Charlie025x <charliealonso2001@gmail.com>
2022-03-18 11:54:01 -0500

    turned app.js to a class component, added initial state, changed html classes to classNames

--- src/App.js
+++ src/App.js
@@ -1,64 +1,76 @@
+// import { render } from "@testing-library/react";
+import { Component } from "react";
 import logo from "./images/logo.svg";
 
-function App() {
-  return (
-    <>
-      <img class="logo" src={logo} alt="logo" />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bill: "",
+      tip: "",
+      numOfPeople: "",
+    };
+  }
+  render() {
+    return (
+      <>
+        <img className="logo" src={logo} alt="logo" />
 
-      <div class="calculator-wrapper">
-        <div class="input-wrapper">
-          <h2>Bill</h2>
-          <input type="text" name="" id="" value="142.55" />
+        <div className="calculator-wrapper">
+          <div className="input-wrapper">
+            <h2>Bill</h2>
+            <input type="text" name="bill" value="142.55" />
 
-          <h2>Select Tip %</h2>
-          <div class="btn-tip-container">
-            <button class="btn-tip">5%</button>
-            <button class="btn-tip">10%</button>
-            <button class="btn-tip btn-active">15%</button>
-            <button class="btn-tip">25%</button>
-            <button class="btn-tip">50%</button>
-            <button class="btn-tip-custom">Custom</button>
+            <h2>Select Tip %</h2>
+            <div className="btn-tip-container">
+              <button className="btn-tip">5%</button>
+              <button className="btn-tip">10%</button>
+              <button className="btn-tip btn-active">15%</button>
+              <button className="btn-tip">25%</button>
+              <button className="btn-tip">50%</button>
+              <button className="btn-tip-custom">Custom</button>
+            </div>
+
+            <h2>Number of People</h2>
+            <input type="text" name="" id="" value="5" />
           </div>
 
-          <h2>Number of People</h2>
-          <input type="text" name="" id="" value="5" />
+          <div className="answer-wrapper">
+            <div className="tip-category">
+              <p>
+                Tip Amount <span>/ person</span>
+              </p>
+              <p className="tip-result">$4.27</p>
+            </div>
+
+            <div className="tip-category">
+              <p>
+                Total <span>/ person</span>
+              </p>
+              <p className="tip-result">$32.79</p>
+            </div>
+
+            <button className="btn-reset">Reset</button>
+          </div>
         </div>
 
-        <div class="answer-wrapper">
-          <div class="tip-category">
-            <p>
-              Tip Amount <span>/ person</span>
-            </p>
-            <p class="tip-result">$4.27</p>
-          </div>
-
-          <div class="tip-category">
-            <p>
-              Total <span>/ person</span>
-            </p>
-            <p class="tip-result">$32.79</p>
-          </div>
-
-          <button class="btn-reset">Reset</button>
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://charlie-alonso.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Charlie Alonso
+          </a>
         </div>
-      </div>
-
-      <div class="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by{" "}
-        <a
-          href="https://charlie-alonso.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Charlie Alonso
-        </a>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
